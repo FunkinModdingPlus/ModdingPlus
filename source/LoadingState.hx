e3501e7ab1d2325bab12188772aa0dc0e6549b82
Axy <85826200+AxyGitPoggers@users.noreply.github.com>
2021-08-09 15:30:59 +0100

    Update LoadingState.hx

--- source/LoadingState.hx
+++ source/LoadingState.hx
@@ -15,18 +15,6 @@ class LoadingState extends FlxState {
         
     }
     override function create() {
-		var titletext = new FlxText(0, 20, 0, "HEY!", 64);
-		titletext.screenCenter(X);
-		var paragraph = new FlxText(0, 120, FlxG.width / 1.5,
-			'Charting is considered deprecated on 4.0.0, and may be removed. Any issues on github about the Charting state will be ignored and closed. Please consider using Funkin Vortex instead as it has more support. \n \n press space to continue',
-			32);
-        add(titletext);
-        paragraph.screenCenter(X);
-        add(paragraph);
-    }
-    override function update(elapsed:Float) {
-        if (FlxG.keys.justPressed.SPACE) {
-            FlxG.switchState(new ChartingState());
-        }
+	FlxG.switchState(new ChartingState());
     }
 }
